d73232c77951ef5d3d82c0bc768c8fda71b76164
Jessica Paola Barbosa de Moraes <Jessica.Moraes@ibm.com>
2023-12-21 13:52:21 -0300

    Feature: create logic in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+// app.component.ts
+
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StateService } from './services/state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'rich-pet';
+export class AppComponent implements OnDestroy {
+  currentPage?: string;
+  private subscription: Subscription;
+
+  constructor(private stateService: StateService) {
+    this.subscription = stateService.currentPage$.subscribe((page) => {
+      if (this.shouldShowModal()) {
+        this.seeModal();
+      } else {
+        this.currentPage = page;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  reset() {
+    this.stateService.reset();
+  }
+
+  public shouldShowModal(): boolean {
+    //create logic padlock
+    return false;
+  }
+
+  private seeModal() {
+    console.log('show modal');
+  }
+
+  confirmModal() {
+    // this.currentPage = this.stateService.currentPage$.value;
+  }
 }
