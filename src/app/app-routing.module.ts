6838f78bb135ef93bdc65ff0e0d5e280ef769b78
Jessica Paola Barbosa de Moraes <Jessica.Moraes@ibm.com>
2023-12-21 13:24:57 -0300

    Feature: Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Port1Component } from './port1/port1.component';
+import { Port2Component } from './port2/port2.component';
+import { Port3Component } from './port3/port3.component';
+import { CatsComponent } from './cats/cats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'port1', component: Port1Component },
+  { path: 'port2', component: Port2Component },
+  { path: 'port3', component: Port3Component },
+  { path: 'cats', component: CatsComponent },
+  { path: '', redirectTo: '/port1', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
